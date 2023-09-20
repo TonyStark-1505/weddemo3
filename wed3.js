7ac1d29abc957cafbcee501d5b8376bab0972dd5
TonyStark-1505 <vanttgch211142@fpt.edu.vn>
2023-09-20 17:30:03 +0700

    greenwicvnh

--- wed3.js
+++ wed3.js
@@ -4,21 +4,21 @@ const fs = require('fs')
 const port = process.env.PORT||3000
 const server = http.createServer((req, res) => {
    if (req.url === '/') {
-      fs.readFile('index.html', (err, data) => {
+      fs.readFile('demo.html', (err, data) => {
          if (err) 
             console.error(err)
          else 
             res.end(data)
       })
    } else if (req.url === '/hello') {
-      fs.readFile('hello.html', (err, data) => {
+      fs.readFile('index.html', (err, data) => {
          if (err)
             console.error(err)
          else
             res.end(data)
       })
    } else if (req.url === '/login') {
-      fs.readFile('login.html', (err, data) => {
+      fs.readFile('contact.html', (err, data) => {
          if (err)
             console.error(err)
          else
